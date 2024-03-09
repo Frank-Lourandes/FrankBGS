e5ed0f89f1ea4b80d6d9f7bc1f4781f5ca6a16bb
Frank。 <67760290+Frank-Lourandes@users.noreply.github.com>
2024-03-09 13:31:46 -0300

    First Commit with ALS4, CityPark and base skating movement.
    ALS4 (Advanced Locomotion System version 4) is being used, as well as the City Park Environment Collection, both from the Unreal Engine Marketplace. The base movement for the skating was taking from "Interaction Isn't Explicit".

---
+++ Source/FrankBGS/FrankBGS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FrankBGS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FrankBGS, "FrankBGS" );
